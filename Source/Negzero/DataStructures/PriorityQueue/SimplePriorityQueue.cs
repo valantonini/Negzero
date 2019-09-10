4824a3a6ed92f40a9fc5d36c463ac01f7f816ea5
val <val.antonini@gmail.com>
2019-09-11 08:05:08 +1000

    comparer constructor for priority queue

--- Source/Negzero/DataStructures/PriorityQueue/SimplePriorityQueue.cs
+++ Source/Negzero/DataStructures/PriorityQueue/SimplePriorityQueue.cs
@@ -1,8 +1,19 @@
+using System.Collections.Generic;
+
 namespace Negzero.DataStructures.PriorityQueue
 {
     public class SimplePriorityQueue<T> : IPriorityQueue<T>
     {
+        private readonly IComparer<T> _comparer;
+
         public int Count => throw new System.NotImplementedException();
+
+        public SimplePriorityQueue() : this(Comparer<T>.Default) { }
+
+        public SimplePriorityQueue(IComparer<T> comparer)
+        {
+            _comparer = comparer;
+        }
 
         public void Clear()
         {
